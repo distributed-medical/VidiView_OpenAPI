28b1649645c4188aa4cc3ce0e8bcf263a994b40a
Björn Lundmark <bjorn@distributedmedical.com>
2024-01-02 17:06:27 +0100

    Fixed more comile errors

--- VidiView.Api.DataModel/Exceptions/E1034_ConcurrentUpdateException.cs
+++ VidiView.Api.DataModel/Exceptions/E1034_ConcurrentUpdateException.cs
@@ -2,6 +2,8 @@
 
 public class E1034_ConcurrentUpdateException : VidiViewException
 {
+    public object? UpdatedInstance { get; set; }
+
     public E1034_ConcurrentUpdateException(string message)
         : base(message)
     {
