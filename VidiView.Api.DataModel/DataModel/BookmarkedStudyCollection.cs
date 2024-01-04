dc07a78136b0d8d80941232612be985ec26c3609
Björn Lundmark <bjorn@distributedmedical.com>
2024-01-04 08:39:30 +0100

    Fixed tests for new refactored api

---
+++ VidiView.Api.DataModel/DataModel/BookmarkedStudyCollection.cs
@@ -0,0 +1,33 @@
+﻿namespace VidiView.Api.DataModel;
+
+public record BookmarkedStudyCollection
+{
+    /// <summary>
+    /// Number of items in this collection
+    /// </summary>
+    public int Count { get; init; }
+
+    public DateTime FromDate { get; init; }
+    public DateTime ToDateExclusive { get; init; }
+    public Guid? DepartmentId { get; init; }
+    public int MaximumHits { get; init; }
+
+    /// <summary>
+    /// The items
+    /// </summary>
+    public BookmarkedStudy[] Items => Embedded.Studies;
+
+    /// <summary>
+    /// Any HAL Rest links associated with this collection
+    /// </summary>
+    [JsonPropertyName("_links")]
+    public LinkCollection? Links { get; init; }
+
+    [JsonPropertyName("_embedded")]
+    public EmbeddedArray Embedded { get; init; }
+
+    public record EmbeddedArray
+    {
+        public BookmarkedStudy[] Studies { get; init; }
+    }
+}
--- VidiView.Api.DataModel/DataModel/RecentlyUsedStudy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VidiView.Api.DataModel;
-
-public record RecentlyUsedStudy : Study
-{
-    /// <summary>
-    /// The time this study was last reviewed by the current user
-    /// </summary>
-    public DateTimeOffset LastReviewDate { get; init; }
-}
