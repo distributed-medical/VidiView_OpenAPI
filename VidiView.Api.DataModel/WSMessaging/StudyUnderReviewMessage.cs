b0a52d05d0769b8b7bf0bb4a374846c2bd7543bc
Björn Lundmark <bjorn@distributedmedical.com>
2024-07-08 11:23:52 +0200

    Updated review message

--- VidiView.Api.DataModel/WSMessaging/StudyUnderReviewMessage.cs
+++ VidiView.Api.DataModel/WSMessaging/StudyUnderReviewMessage.cs
@@ -26,6 +26,12 @@ public class StudyUnderReviewMessage : IWSReply
 
 public class ReviewingUser
 {
-    public User User { get; init; }
+    /// <summary>
+    /// User currently reviewing this study
+    /// </summary>
+    public IdAndName User { get; init; }
+    /// <summary>
+    /// Device used to review study
+    /// </summary>
     public ClientDevice Device { get; init; }
 }
