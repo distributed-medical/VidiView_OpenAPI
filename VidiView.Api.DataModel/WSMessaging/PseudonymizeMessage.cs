34889b8fe955ac0a8d35d34b52541251002c8246
Björn Lundmark <bjorn@distributedmedical.com>
2024-07-25 14:14:26 +0200

    Added pseudonymize message

---
+++ VidiView.Api.DataModel/WSMessaging/PseudonymizeMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace VidiView.Api.WSMessaging;
+
+/// <summary>
+/// Request pseudonymization on messages 
+/// </summary>
+public class PseudonymizeMessage : IWSMessage
+{
+    public PseudonymizeMessage()
+    {
+        // Maybe it seems odd to use the ToString() here instead
+        // of FullName, but the ToString will not return assembly qualified name
+        // for generic type parameters
+        MessageType = this.GetType().ToString();
+        MessageId = Guid.NewGuid().ToString("N");
+    }
+
+    public string MessageType { get; init; }
+    public string MessageId { get; init; }
+
+    public bool Enable { get; init; }
+}
