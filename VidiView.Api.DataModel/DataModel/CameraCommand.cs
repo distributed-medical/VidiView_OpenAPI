e0b3af3a8254832c20911accecba47b0332fcc13
Björn Lundmark <bjorn@distributedmedical.com>
2024-10-11 09:45:13 +0200

    Added camera command

---
+++ VidiView.Api.DataModel/DataModel/CameraCommand.cs
@@ -0,0 +1,47 @@
+﻿namespace VidiView.Api.DataModel;
+
+/// <summary>
+/// Execute a camera command
+/// </summary>
+public record CameraCommand
+{
+    /// <summary>
+    /// Select a specific preset
+    /// </summary>
+    public Guid? PresetId { get; init; }
+
+    /// <summary>
+    /// Pan camera relative current position and visible area
+    /// </summary>
+    /// <remarks>Current position is 0.5, the left edge of the visible area is 0.0 and the right edge is 1.0</remarks>
+    public double? PanRelative { get; init; }
+
+    /// <summary>
+    /// Pan camera to absolute position
+    /// </summary>
+    /// <remarks>Leftmost position of camera is 0.0 and rightmost 1.0</remarks>
+    public double? PanAbsolute { get; init; }
+
+    /// <summary>
+    /// Tilt camera relative current position and visible area
+    /// </summary>
+    /// <remarks>Current position is 0.5, the top edge of the visible area is 1.0 and the bottom edge is 0.0</remarks>
+    public double? TiltRelative { get; init; }
+
+    /// <summary>
+    /// Tilt camera to absolute position
+    /// </summary>
+    /// <remarks>Top position of camera is 1.0 and bottom is 0.0</remarks>
+    public double? TiltAbsolute { get; init; }
+
+    /// <summary>
+    /// Set camera zoom to absolute value.
+    /// </summary>
+    /// <remarks>Minimum zoom is 0.0 (wide), and maximum supported zoom is 1.0 (tele)</remarks>
+    public double? ZoomAbsolute { get; init; }
+
+    /// <summary>
+    /// Start/stop a pan/tilt/zoom operation
+    /// </summary>
+    public CameraDriveEnum? Drive { get; init; }
+}
---
+++ VidiView.Api.DataModel/DataModel/CameraDriveEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace VidiView.Api.DataModel;
+
+[JsonConverter(typeof(StringEnumConverterEx<CameraDriveEnum>))]
+[Flags]
+public enum CameraDriveEnum 
+{
+    None = 0x0,
+    PanLeft = 0x1,
+    PanRight = 0x2,
+    TiltUp = 0x4,
+    TiltDown = 0x8,
+    ZoomWide = 0x16, // Zoom out
+    ZoomTele = 0x32, // Zoom in
+}
