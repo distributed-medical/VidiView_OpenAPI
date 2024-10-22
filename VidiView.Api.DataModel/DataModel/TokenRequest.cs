1b393a9c1787685ab6280b45abac05b0cbf3b734
Björn Lundmark <bjorn@distributedmedical.com>
2024-10-22 09:06:17 +0200

    Added OTP support for token request

--- VidiView.Api.DataModel/DataModel/TokenRequest.cs
+++ VidiView.Api.DataModel/DataModel/TokenRequest.cs
@@ -8,16 +8,21 @@ public class TokenRequest
     public TimeSpan? Lifetime { get; init; }
 
     /// <summary>
-    /// Update the scope. 
-    /// Note! You cannot extend the scope beyond what is already granted in the current session
-    /// </summary>
-    public string? Scope { get; set; }
-
-    /// <summary>
     /// If token is intended for another application, specify
     /// the application id here
     /// </summary>
     public Guid? AppId { get; set; }
+
+    /// <summary>
+    /// If set, the returned token will be of one-time-password type
+    /// </summary>
+    public bool OtpToken { get; set; } = false;
+
+    /// <summary>
+    /// Update the scope. 
+    /// Note! You cannot extend the scope beyond what is already granted in the current session
+    /// </summary>
+    public string? Scope { get; set; }
 
     /// <summary>
     /// If supplied, this will add a contribute restriction with access to the specified studies only
