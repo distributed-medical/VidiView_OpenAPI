7efa2e27aa6b9ee85177519cce616477f95894e9
Björn Lundmark <bjorn@distributedmedical.com>
2024-11-18 14:42:09 +0100

    Added support for audit log events

---
+++ VidiView.Api.DataModel/DataModel/AuditEventCollection.cs
@@ -0,0 +1,31 @@
+﻿namespace VidiView.Api.DataModel;
+
+public record AuditEventCollection
+{
+    /// <summary>
+    /// Number of items in this collection
+    /// </summary>
+    public int Count { get; init; }
+
+    public DateTime FromDate { get; init; }
+    public DateTime ToDateExclusive { get; init; }
+
+    /// <summary>
+    /// The events
+    /// </summary>
+    public AuditEvent[] Items => Embedded.Events;
+
+    /// <summary>
+    /// Any HAL Rest links associated with this collection
+    /// </summary>
+    [JsonPropertyName("_links")]
+    public LinkCollection? Links { get; init; }
+
+    [JsonPropertyName("_embedded")]
+    public EmbeddedArray Embedded { get; init; }
+
+    public record EmbeddedArray
+    {
+        public AuditEvent[] Events { get; init; }
+    }
+}
---
+++ VidiView.Api.DataModel/DataModel/AuditEventId.cs
@@ -0,0 +1,22 @@
+﻿namespace VidiView.Api.DataModel;
+
+/// <summary>
+/// Event id information
+/// </summary>
+public record AuditEventId
+{
+    /// <summary>
+    /// Event id
+    /// </summary>
+    public int EventId { get; init; }
+
+    /// <summary>
+    /// Event category
+    /// </summary>
+    public string Category { get; init; }
+
+    /// <summary>
+    /// Event name
+    /// </summary>
+    public string Name { get; init; }
+}
