f66fd15d748507ae5344944f9d840ec6cef887be
Björn <bjorn@distributedmedical.com>
2025-01-21 15:45:08 +0100

    Updated service host model

---
+++ VidiView.Api.Configuration/DataModel/AnnotationOptionEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidiView.Api.Configuration.DataModel;
+
+[JsonConverter(typeof(StringEnumConverterEx<AnnotationOption>))]
+public enum AnnotationOption
+{
+    Unknown,
+
+    /// <summary>
+    /// Skip annotations when exporting
+    /// </summary>
+    Skip,
+
+    /// <summary>
+    /// Create a Structured report for the annotaions
+    /// </summary>
+    SR,
+
+    /// <summary>
+    /// Burn annotation in image
+    /// </summary>
+    Burn
+}
--- VidiView.Api.Configuration/DataModel/ExportConfiguration.cs
+++ VidiView.Api.Configuration/DataModel/ExportConfiguration.cs
@@ -1,36 +1,10 @@
 ﻿namespace VidiView.Api.Configuration.DataModel;
-
-[JsonConverter(typeof(StringEnumConverterEx<AnnotationOption>))]
-public enum AnnotationOption
-{
-    Unknown,
-
-    /// <summary>
-    /// Skip annotations when exporting
-    /// </summary>
-    Skip,
-
-    /// <summary>
-    /// Create a Structured report for the annotaions
-    /// </summary>
-    SR,
-
-    /// <summary>
-    /// Burn annotation in image
-    /// </summary>
-    Burn
-}
 public record ExportConfiguration
 {
     /// <summary>
-    /// This archive accepts snapshot images
+    /// The content types accepted. May use wildcard (i.e. image/ or video/)
     /// </summary>
-    public bool AcceptSnapshot { get; init; }
-
-    /// <summary>
-    /// This archive accepts video images
-    /// </summary>
-    public bool AcceptVideo { get; init; }
+    public string[]? ContentType { get; init; }
 
     /// <summary>
     /// Annotation handling
@@ -40,21 +14,23 @@ public record ExportConfiguration
     /// <summary>
     /// Maximum video duration for export
     /// </summary>
+    /// <remarks>If TimeSpan.Zero, no limit will be enforced</remarks>
     public TimeSpan MaxVideoDuration { get; init; }
 
     /// <summary>
-    /// Reduce videoresolution to specified number of lines
+    /// Reduce video resolution to specified number of lines
     /// </summary>
+    /// <remarks>If 0, no reduction will be enforced</remarks>
     public int MaxVideoResolution { get; init; }
 
     /// <summary>
     /// Reduce bit rate to the specified bit rate
     /// </summary>
-    /// <remarks>This is defined in Kbit/s</remarks>
+    /// <remarks>This is defined in Kbit/s. If 0, no reduction will be enforced</remarks>
     public int MaxVideoBitRate { get; init; }
 
     /// <summary>
     /// Enable legacy large file support. This will generate non-conformant Dicom files
     /// </summary>
-    public bool LegacyLargeFileSupport { get; set; } = false;
+    public bool LegacyLargeFileSupport { get; init; }
 }
