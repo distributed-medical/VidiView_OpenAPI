98a3999c7506229ad3e992d9bea1ab1d4386077a
Björn <bjorn@distributedmedical.com>
2025-01-22 12:55:08 +0100

    Add error definition

---
+++ VidiView.Api.Helpers/Exceptions/E1718_ReferenceViolationException.cs
@@ -0,0 +1,9 @@
+﻿namespace VidiView.Api.Exceptions;
+
+public class E1718_ReferenceViolationException : VidiViewException
+{
+    public E1718_ReferenceViolationException(string message)
+        : base(message)
+    {
+    }
+}
