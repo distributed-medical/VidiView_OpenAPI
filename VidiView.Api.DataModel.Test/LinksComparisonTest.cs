3e0f738b63fc80fbfe5bea7ed6f45ae7d019cba1
Björn <bjorn@distributedmedical.com>
2025-02-03 13:05:39 +0100

    Added additional null unit test

--- VidiView.Api.DataModel.Test/LinksComparisonTest.cs
+++ VidiView.Api.DataModel.Test/LinksComparisonTest.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using VidiView.Api.DataModel;
-
 namespace VidiView.Api.DataModel.Test;
 
 [TestClass]
@@ -55,6 +46,27 @@ public class LinksComparisonTest
         };
         ((IDictionary<string, Link>)mf2.Links)[Rel.ActiveSessions] = new Link { Href = "http://test.se/uri/", Templated = false };
         ((IDictionary<string, Link>)mf2.Links)[Rel.Self] = new Link { Href = "http://test.se/uri/2", Templated = true }; // Note URI difference
+
+        // Act
+        Assert.AreNotEqual(mf1, mf2);
+    }
+
+    [TestMethod]
+    public void Compare_With_Null()
+    {
+        // Arrange
+        var mf1 = new MediaFile
+        {
+            Status = MediaFileStatus.Verified,
+            Links = new LinkCollection()
+        };
+        ((IDictionary<string, Link>)mf1.Links)[Rel.ActiveSessions] = new Link { Href = "http://test.se/uri/", Templated = false };
+        ((IDictionary<string, Link>)mf1.Links)[Rel.Self] = new Link { Href = "http://test.se/uri/1", Templated = true };
+
+        var mf2 = new MediaFile
+        {
+            Status = MediaFileStatus.Verified,
+        };
 
         // Act
         Assert.AreNotEqual(mf1, mf2);
