4b9aa3422b92e6010099f317766687603e60908a
Björn <bjorn@distributedmedical.com>
2025-07-18 10:41:00 +0200

    Added low loevel services required for server communication

---
+++ VidiView.Api.Helpers/Exceptions/E1510_DecryptDataException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidiView.Api.Exceptions;
+public class E1510_DecryptDataException : VidiViewException
+{
+    public E1510_DecryptDataException(string message)
+        : base(message)
+    {
+        ErrorCode = 1510;
+    }
+    public E1510_DecryptDataException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+        ErrorCode = 1510;
+    }
+}
