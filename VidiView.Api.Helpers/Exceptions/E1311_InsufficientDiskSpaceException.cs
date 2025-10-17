a5c6bcd9b8071618fd2f7c4b004b37552b71d255
Björn <bjorn@distributedmedical.com>
2025-10-17 08:59:29 +0200

    Added exceptions

---
+++ VidiView.Api.Helpers/Exceptions/E1311_InsufficientDiskSpaceException.cs
@@ -0,0 +1,10 @@
+﻿namespace VidiView.Api.Exceptions;
+
+public class E1311_InsufficientDiskSpaceException : VidiViewException
+{
+    public E1311_InsufficientDiskSpaceException(string message)
+        : base(message)
+    {
+        ErrorCode = 1311;
+    }
+}
---
+++ VidiView.Api.Helpers/Exceptions/E1407_UnsupportedVersionException.cs
@@ -0,0 +1,10 @@
+﻿namespace VidiView.Api.Exceptions;
+
+public class E1407_UnsupportedVersionException : VidiViewException
+{
+    public E1407_UnsupportedVersionException(string message)
+        : base(message)
+    {
+        ErrorCode = 1407;
+    }
+}
