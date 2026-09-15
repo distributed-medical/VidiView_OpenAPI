6deb956cf04aea584f3792d8bff5299039ec7b59
BjörnLundmark <bjorn@distributedmedical.com>
2026-09-15 10:58:32 +0200

    Added type specifics

---
+++ VidiView.Api.DataModel/TypeSpecifics/MicrophoneSoundPressureLevelCalibrationSpecifics.cs
@@ -0,0 +1,43 @@
+﻿namespace VidiView.Api.DataModel;
+
+/// <summary>
+/// Specifics used when calibrating a microphone to determine its sensitivity
+/// </summary>
+[ExcludeFromCodeCoverage]
+public record MicrophoneSoundPressureLevelCalibrationSpecifics
+{
+    /// <summary>
+    /// This is the intended media type for this specific data, i.e. the media type that should be used when storing this data in a media file
+    /// </summary>
+    public static Guid IntendedMediaType => WellKnownMediaType.MicrophoneSoundPressureLevelCalibration;
+
+    /// <summary>
+    /// Signal used for calibration, e.g. "Sine wave 1kHz", "Pink noise" etc
+    /// </summary>
+    public string CalibrationSignal { get; init; }
+
+    /// <summary>
+    /// Reference level for calibration, in dB SPL, e.g. 94.0, 114.0 
+    /// </summary>
+    public double ReferenceLevel { get; init; }
+
+    /// <summary>
+    /// Id of the microphone being calibrated
+    /// </summary>
+    public string MicrophoneId { get; init; }
+
+    /// <summary>
+    /// Sample rate used when measuring the microphone calibration, in Hz, e.g. 44100.0, 48000.0
+    /// </summary>
+    public double SampleRate { get; init; }
+
+    /// <summary>
+    /// The device volume level used when measuring the microphone calibration (0.0 - 100.0)
+    /// </summary>
+    public double VolumeLevel { get; init; }
+
+    /// <summary>
+    /// The calculated sensitivity of the microphone, in mV/Pa
+    /// </summary>
+    public double Sensitivity { get; init; }
+}
---
+++ VidiView.Api.DataModel/TypeSpecifics/VoiceRecordingSpecifics.cs
@@ -0,0 +1,23 @@
+﻿namespace VidiView.Api.DataModel;
+
+/// <summary>
+/// The microphone sensitivity to use in a specific media file, to determine the actual sound pressure level (SPL) in dB SPL of the contained audio signal
+/// </summary>
+[ExcludeFromCodeCoverage]
+public record VoiceRecordingSpecifics
+{
+    /// <summary>
+    /// This is the intended media type for this specific data, i.e. the media type that should be used when storing this data in a media file
+    /// </summary>
+    public static Guid IntendedMediaType => WellKnownMediaType.VoiceRecording;
+
+    /// <summary>
+    /// The media file id that contains the microphone calibration data
+    /// </summary>
+    public Guid CalibrationId { get; init; }
+
+    /// <summary>
+    /// Microphone sensitivity, in mV/Pa
+    /// </summary>
+    public double Sensitivity { get; init; }
+}
